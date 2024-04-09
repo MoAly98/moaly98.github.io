29fc9229008abe036404e2b64768d6b0c26ca8e2
maly <mohamedaymanali@gmail.com>
2024-04-09 17:50:32 +0100

    remove uncessary classes, JS code affecting them. Writing section now same structure as projects section. Would be nice to implement a filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,35 +157,6 @@
   }
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
-  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
@@ -216,35 +187,35 @@
   /**
    * Initiate writing lightbox
    */
-  const writingLightbox = GLightbox({
-    selector: '.writing-lightbox'
-  });
+//   const writingLightbox = GLightbox({
+//     selector: '.writing-lightbox'
+//   });
 
   /**
    * Initiate writing details lightbox
    */
-  const writingDetailsLightbox = GLightbox({
-    selector: '.writing-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
+//   const writingDetailsLightbox = GLightbox({
+//     selector: '.writing-details-lightbox',
+//     width: '90%',
+//     height: '90vh'
+//   });
 
   /**
    * writing details slider
    */
-  new Swiper('.writing-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+//   new Swiper('.writing-details-slider', {
+//     speed: 400,
+//     loop: true,
+//     autoplay: {
+//       delay: 5000,
+//       disableOnInteraction: false
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//       clickable: true
+//     }
+//   });
 
   /**
    * Initiate Pure Counter
