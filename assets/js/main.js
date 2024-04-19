dc92ef74ad9b4aa136716540de73aeb02df98f69
Nouranaymanaly <nouranaymanali@gmail.com>
2024-04-19 15:57:04 +0200

    fixing buttons and spacing between rows

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,34 +155,80 @@
       }
     })
   }
+// /** filter */
+// filterSelection("all");
+
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("filterDiv");
+//   if (c == "all") c = "";
+//   for (i = 0; i < x.length; i++) {
+//     w3RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//   }
+// }
+
+// function w3AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
+
+// function w3RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
+
+// // Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
 
   /**
    * Porfolio isotope and filter
    */
-  // window.addEventListener('load', () => {
-  //   let writingContainer = select('.writing-container');
-  //   if (writingContainer) {
-  //     let writingIsotope = new Isotope(writingContainer, {
-  //       itemSelector: '.writing-item',
-  //       layoutMode: 'fitRows'
-  //     });
+  window.addEventListener('load', () => {
+    let writingContainer = select('.writing-container');
+    if (writingContainer) {
+      let writingIsotope = new Isotope(writingContainer, {
+        itemSelector: '.writing-item',
+        layoutMode: 'fitRows'
+      });
 
-  //     let writingFilters = select('#writing-filters li', true);
+      let writingFilters = select('#writing-filters li', true);
 
-  //     on('click', '#writing-filters li', function(e) {
-  //       e.preventDefault();
-  //       writingFilters.forEach(function(el) {
-  //         el.classList.remove('filter-active');
-  //       });
-  //       this.classList.add('filter-active');
+      on('click', '#writing-filters li', function(e) {
+        e.preventDefault();
+        writingFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
 
-  //       writingIsotope.arrange({
-  //         filter: this.getAttribute('data-filter')
-  //       });
-  //     }, true);
-  //   }
+        writingIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+      }, true);
+    }
 
-  // });
+  });
 
   /**
    * Initiate writing lightbox
